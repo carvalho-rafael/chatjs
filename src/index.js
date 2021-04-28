08fd148825ca07952fae6007947b745c016e01bc
Rafael Carvalho <rafaelvfcarvalho@gmail.com>
2021-04-28 13:43:40 -0300

    log service work

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { registerServiceWorker } from './serviceWorker';
 
+registerServiceWorker();
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-registerServiceWorker();
